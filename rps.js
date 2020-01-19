421d7f5fc6da626c5bd16497b84be5554de1238b
ShadowofZeus <shadowofzeus811@gmail.com>
2020-01-19 18:31:55 +0200

    Revision-1 of the Rock Paper Scissors Game

--- rps.js
+++ rps.js
@@ -25,49 +25,60 @@ function computerPlay()
 
 function OneRound(playerSelection,computerSelection)
 {
+
 	if ((playerSelection ==='rock') && (computerSelection === 'rock'))
 	{
-		console.log('This is a tie amigos - shame');
+		//console.log('This is a tie amigos - shame');
+		return 'draw';
+	
 	}
 
 	else if ((playerSelection === 'rock') && (computerSelection === 'paper'))
 	{
-		console.log('computer-amigo WINS!');
+		//console.log('computer-amigo WINS!');
+		return 'computer';
 	}
 
 	else if ((playerSelection === 'rock') && (computerSelection === 'scissors'))
 	{
-		console.log('human-amigo WINS!');
+		//console.log('human-amigo WINS!');
+		return 'human';
 	}
 
 	else if ((playerSelection === 'paper') && (computerSelection === 'rock'))
 	{
-		console.log('human-amigo WINS!');
+		//console.log('human-amigo WINS!');
+		return 'human';
 	}
 
 	else if ((playerSelection === 'paper') && (computerSelection === 'paper'))
 	{
-		console.log('This is a tie amigos - shame');
+		//console.log('This is a tie amigos - shame');
+		return 'draw';
 	}
 
 	else if ((playerSelection === 'paper') && (computerSelection === 'scissors'))
 	{
-		console.log('computer-amigo WINS!');
+		//console.log('computer-amigo WINS!');
+		return 'computer';
 	}
 
 	else if ((playerSelection === 'scissors') && (computerSelection === 'rock'))
 	{
-		console.log('computer-amigo WINS!');
+		//console.log('computer-amigo WINS!');
+		return 'computer';
 	}
 
 	else if ((playerSelection === 'scissors') && (computerSelection === 'paper'))
 	{
-		console.log('human-amigo WINS!!');
+		//console.log('human-amigo WINS!!');
+		return 'human';
 	}
 
 	else if ((playerSelection === 'scissors') && (computerSelection === 'scissors'))
 	{
-		console.log('This is a tie amigos - shame');
+		//console.log('This is a tie amigos - shame');
+		return 'draw';
 	}
 
 	else 
@@ -76,11 +87,68 @@ function OneRound(playerSelection,computerSelection)
 	}
 }
 
-const value_comp = computerPlay();
-const value_human = String(prompt('Select either rock,paper or scissors please: '));   //making sure its a string
-const valueh_lower = value_human.toLowerCase();											//making sure it always a lower case.
-console.log(valueh_lower); 																//chekcing code credibility
-console.log(value_comp);																//chekcing code credibility
+function gameFive(rounds=5)
+{
+	let human = 0;
+	let computer = 0;
+	textArray = ['Round-1','Round-1','Round-1',];
 
-OneRound(valueh_lower,value_comp);
+	for(i=0; i<rounds; i++)
+	{
+		const value_comp = computerPlay();
+		const value_human = String(prompt('Select either rock,paper or scissors please: '));   //making sure its a string
+		const valueh_lower = value_human.toLowerCase();
+		console.log(valueh_lower); 																//chekcing code credibility
+		console.log(value_comp);																//chekcing code credibility
+		billboard = OneRound(valueh_lower,value_comp);
+
+		switch (billboard) 
+		{
+		  case 'draw':
+		  	human++;
+		  	computer++;
+		  	console.log(`Round-${i+1} Results:`);
+		    console.log(`Human: ${human} and Computer: ${computer}`);
+		    break;
+		  case 'computer':
+		    computer++;
+		    console.log(`Round-${i+1} Results:`);
+		    console.log(`Human: ${human} and Computer: ${computer}`);
+		    break;
+		  case 'human':
+		    human++;
+		    console.log(`Round-${i+1} Results:`);
+		    console.log(`Human: ${human} and Computer: ${computer}`);
+		    break;
+		   
+		  default:
+		    console.log('No one is winning today');
+		    break;
+		}
+	}
+
+	if(human>computer)
+	{
+		
+		alert('Human is KING!!');
+	}
+
+	else if(computer>human)
+	{
+		
+		alert('Computer is KING!!');
+	}
+
+	else
+	{
+		alert('No winner due to invalid input from users')
+	}
+}
+
+//document.getElementById("begin").onclick = gameFive;
+
+										
+
+
+
 
